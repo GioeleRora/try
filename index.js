78d22893716c993d69b1c0c17a9d00f60446438b
Gioele <gioelerora05@gmail.com>
2025-03-29 13:18:25 +0100

    Initial commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const PORT = 4000;
 
 app.get('/home', (req, res) => {
-    res.status(200).json('Welcome, your app is working well');
+    res.status(200).json('Welcome, your app is cacca well');
 });
 
 app.get('/', (req, res) => {
-    res.status(200).json('Welcome to the home page');
+    res.status(200).json('Welcome to the cacca page');
 });
 
 app.listen(PORT, () => {
